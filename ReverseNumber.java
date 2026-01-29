cbbcd8a130b440156016479cdac46489947e06f9
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-01-30 01:10:03 +0530

    changed reversed number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -10,12 +10,13 @@ class ReverseNumber{
     }
 
     static void reverseNumber(int n){
-        int count = 0;
+        int revNum = 0;
         while(n>0){
             int lastDigit = n%10;
-            System.out.print(lastDigit);
             n = n/10;
+            revNum = (revNum * 10)+lastDigit;
         }
+        System.out.println("The reversed number is : "+revNum);
 
     }
 }
