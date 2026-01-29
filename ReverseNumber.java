a70c85de3bd0232b1af3092cb4a35f8df183f38c
Amardeep1811 <amardeepsharma95238@gmail.com>
2026-01-30 01:02:26 +0530

    Reverse number

---
+++ ReverseNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ReverseNumber{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to find its reverse : ");
+        int n = input.nextInt();
+
+        reverseNumber(n);
+    }
+
+    static void reverseNumber(int n){
+        int count = 0;
+        while(n>0){
+            int lastDigit = n%10;
+            System.out.print(lastDigit);
+            n = n/10;
+        }
+
+    }
+}
